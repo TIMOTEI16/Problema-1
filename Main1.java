fa7167157cf8d1d4fb0d816675e9c56c692323c1
Negoi Timotei <106741450+TIMOTEI16@users.noreply.github.com>
2022-06-07 11:23:14 +0300

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number:");
+
+        int num = sc.nextInt();
+
+        int org_num = num;
+
+        int rev = 0;
+
+        while (num !=0) {
+            rev = rev * 10 + num % 10;
+            num = num / 10;
+        }
+        if (org_num == rev) {
+            System.out.println(org_num + "numarul este palindrom");
+        } else {
+            System.out.println(org_num + "numarul nu este palindrom");
+        }
+
+    }
+}
