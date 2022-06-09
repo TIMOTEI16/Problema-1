4c85e5caa5382a9a13765cde837cf2af469357f8
Negoi Timotei <106741450+TIMOTEI16@users.noreply.github.com>
2022-06-09 14:31:54 +0300

    Add files via upload

---
+++ Problema.java
@@ -0,0 +1,66 @@
+import java.text.Format;
+import java.util.Scanner;
+
+
+//PROBLEMA 1
+
+public class Problema {
+    public static void main(String[] args) {
+
+//        System.out.println("Se da String-ul:");
+//        Scanner sca = new Scanner(System.in);
+//
+//        String str = sca.next();
+//        int n = 3;
+//        if ( n > 1 && n < str.length()  ) {
+//            str.substring(str.length() - n  , str.length() );
+//            for (int i = 0; i < n; ++i) {
+//                System.out.print(str.substring(str.length() - n, str.length()));
+//            }
+//
+//        }
+
+
+
+
+        //PROBLEMA 2
+//
+//        int[] sir = {2, 6, 7, 9, 4};
+//        int num = 0, numImp = 0;
+//        for( int i = 0; i <  sir.length; ++i) {
+//        if (sir[i] % 2 == 0 ) {
+//            num++;
+//        }
+//          else {
+//              num = 0;
+//        }
+//        if (num == 3 ) {
+//            break;
+//        }
+//            if(sir[i] % 2 == 1) {
+//            numImp++;
+//            }
+//            else {
+//                numImp = 0;
+//            }
+//            if (numImp == 3 ) {
+//                break;
+//            }
+//        }
+//        if (num == 3 || numImp == 3)
+//            System.out.println(true);
+//        else{
+//            System.out.println(false);
+//        }
+
+       // PROBLEMA 3;
+
+
+
+
+
+
+
+
+    }
+}
